2d22ff91e11f7e123c8b7d94cac260eb4e5d3cf4
Abominavel_Sirius <abominavelsirius@gmail.com>
2024-10-08 09:22:30 -0300

    atualizando

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-let numeroSecreto = 29;
+let numeroSecreto = 5;
 let chute = prompt('Escolha um número entre 1 e 10');
+console.log(numeroSecreto);
 
-alert("diosadisaodisa");
+alert("Bem-vindo ao jogo do número secreto!!!");
 
 
 
 if (numeroSecreto == chute) {
     console.log('Você acertou!');
+} else {
+    alert('ERROU!!!')
 }
 
-if (numeroSecreto != chute) {
-    console.log('Você errou!');
-}
+
