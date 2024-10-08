d8a722cdb66f846b6276c56216ef4107e8dcbd5e
Abominavel_Sirius <abominavelsirius@gmail.com>
2024-10-08 08:48:59 -0300

    add arquivos

---
+++ app.js
@@ -0,0 +1,14 @@
+let numeroSecreto = 29;
+let chute = prompt('Escolha um número entre 1 e 10');
+
+alert("diosadisaodisa");
+
+
+
+if (numeroSecreto == chute) {
+    console.log('Você acertou!');
+}
+
+if (numeroSecreto != chute) {
+    console.log('Você errou!');
+}
