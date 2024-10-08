6820e558ffb2a987692ffb9c506f76ff1b8997fc
Abominavel_Sirius <abominavelsirius@gmail.com>
2024-10-08 09:50:51 -0300

    atualizando

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+alert("Bem-vindo ao jogo do número secreto!!!");
+
 let numeroSecreto = 5;
 let chute = prompt('Escolha um número entre 1 e 10');
 console.log(numeroSecreto);
 
-alert("Bem-vindo ao jogo do número secreto!!!");
+
 
 
 
 if (numeroSecreto == chute) {
-    console.log('Você acertou!');
+    alert(`Você acertou o número secreto ${numeroSecreto}!`);
 } else {
-    alert('ERROU!!!')
+    if (chute > numeroSecreto) {
+        alert(`O número secreto é menor que ${chute}`);
+} else {
+    alert(`O número secreto é maior que ${chute}`);
+}
 }
 
 
