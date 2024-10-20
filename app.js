6da33de8c3c8b905313f3aa2a65623d5e5a696d5
Abominavel_Sirius <abominavelsirius@gmail.com>
2024-10-20 07:45:59 -0300

    finalizando primeira parte

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-alert("Bem-vindo ao jogo do número secreto!!!");
-
-let numeroSecreto = 5;
-let chute = prompt('Escolha um número entre 1 e 10');
+alert('Boas vindas ao jogo do número secreto');
+let numeroMaximo = 10;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-
-
-
-
-if (numeroSecreto == chute) {
-    alert(`Você acertou o número secreto ${numeroSecreto}!`);
-} else {
-    if (chute > numeroSecreto) {
-        alert(`O número secreto é menor que ${chute}`);
-} else {
-    alert(`O número secreto é maior que ${chute}`);
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    // se chute for igual ao número secreto 
+    if(chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else { 
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        // tentativas = tentativas +1;
+        tentativas++;
+    }
 }
-}
-
-
