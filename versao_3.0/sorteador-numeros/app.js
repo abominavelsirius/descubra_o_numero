7e8a27908b891b8381348e2c4e8e0deecc842048
Abominavel_Sirius <abominavelsirius@gmail.com>
2024-10-29 12:44:57 -0300

    tentando resolver

---
+++ versao_3.0/sorteador-numeros/app.js
@@ -0,0 +1,22 @@
+
+
+
+let quantidade = document.getElementById('quantidade').value;
+let de = document.getElementById('de').value;
+let ate = document.getElementById('ate').value;
+
+
+
+function sortear(min, max) {
+    return Math.random() * (max - min) + min;
+
+    
+}
+
+
+
+
+
+function reiniciar() {
+
+}
